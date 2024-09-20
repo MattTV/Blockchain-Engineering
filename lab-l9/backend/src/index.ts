e8cd1cbafddda2900225df885bc221a41ce4b1bf
Matt Volpe <60626843+MattTV@users.noreply.github.com>
2024-09-20 12:55:59 -0700

    lab 9 in progress

---
+++ lab-l9/backend/src/index.ts
@@ -0,0 +1,59 @@
+import express, { Express, Request, Response } from 'express'
+import bodyParser from 'body-parser'
+import Authrite from 'authrite-express'
+
+// Let TypeScript know there is possibly an authrite prop on incoming requests
+declare module 'express-serve-static-core' {
+  interface Request {
+    authrite?: {
+      identityKey: string
+    }
+    certificates?: any
+  }
+}
+
+const app: Express = express()
+const port = 3000
+
+// TODO: Define the server private key and base URL
+const PRIVATE_KEY = '0d7889a0e56684ba795e9b1e28eb906df43454f8172ff3f6807b8cf9464994df'
+const SERVER_URL = `http://localhost:${port}`
+
+// Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// CORS Headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Expose-Headers', '*')
+  res.header('Access-Control-Allow-Private-Network', 'true')
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
+// TODO (Optionally): Configure non-protected routes
+
+// TODO: Configure the express server to use the authrite middleware
+app.use(Authrite.middleware({
+  serverPrivateKey: PRIVATE_KEY,
+  baseUrl: SERVER_URL,
+}))
+
+// TODO: Configure protected route
+app.post('/protected', (req, res) => {
+  res.json({
+    message: 'Your message was received',
+    clientData: req.body,
+  })
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
---
+++ lab-l9/frontend/src/App.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { Button, Typography, Container, CircularProgress } from '@mui/material'
+import { AuthriteClient } from 'authrite-js'
+
+const KEY = '6dcc124be5f382be631d49ba12f61adbce33a5ac14f6ddee12de25272f943f8b'
+const PORT = 3000
+const SERVER_URL: string = 'http://localhost:3000'
+
+const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [response, setResponse] = useState<string | null>(null)
+
+  const handleButtonClick = async () => {
+    // TODO: Create an instance of AuthriteClient and make a signed request to '/protected'
+    
+    setIsLoading(true)
+
+    // Create an Authrite Client instance
+    const authrite = new AuthriteClient({
+      serverURL: SERVER_URL,
+      authriteParams: {
+        clientPrivateKey: KEY,
+      }
+    })
+
+    // Create the body of the request
+    const body = {
+      user: 'Matt',
+      message: 'Rocket League is goated',
+    }
+
+    // Create the request
+    const response = await authrite.createSignedRequest('/protected', {
+      body,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    // Parse the response
+    setResponse(JSON.parse(Buffer.from(response.body).toString('utf8')))
+
+    setIsLoading(false)
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" component="h1" gutterBottom>
+        Frontend App
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleButtonClick}
+        disabled={isLoading}
+      >
+        {isLoading ? <CircularProgress size={24} /> : 'Send Request to Backend'}
+      </Button>
+      {response && (
+        <Typography variant="body1" style={{ marginTop: '20px' }}>
+          Response from backend: {response}
+        </Typography>
+      )}
+    </Container>
+  )
+}
+
+export default App
