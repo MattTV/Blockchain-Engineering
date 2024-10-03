4bd849e197ceb8fabe2b0cf89e1474bdac96088b
Matt Volpe <60626843+MattTV@users.noreply.github.com>
2024-10-03 14:36:00 -0700

    lab 10 finished

--- lab-l10/frontend/src/App.tsx
+++ lab-l10/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Typography, Container, CircularProgress } from '@mui/material'
+import { Button, Typography, Container, CircularProgress, Card, CardContent } from '@mui/material'
 import { AuthriteClient } from 'authrite-js'
 import PacketPay from '@packetpay/js'
 
@@ -10,7 +10,7 @@ const SERVER_URL = `http://localhost:${PORT.toString()}`
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [response, setResponse] = useState<string | null>(null)
-  const [weather, setWeather] = useState<object | null>(null)
+  const [weather, setWeather] = useState<any | null>(null)
 
   const handleButtonClick = async () => {
     // TODO: Create an instance of AuthriteClient and make a signed request to '/protected'
@@ -59,6 +59,7 @@ const App: React.FC = () => {
       )
 
       setWeather(await response.json())
+      console.log(await response.json())
 
     } catch (error) {
 
@@ -97,6 +98,17 @@ const App: React.FC = () => {
         <Typography variant="body1" style={{ marginTop: '20px' }}>
           Response from backend: {response}
         </Typography>
+      )}
+      {weather && (
+        <Card>
+          <CardContent>
+            <Typography variant='h4'>{weather.name} Weather</Typography>
+            <Typography variant='h6'>Temp: {weather.main.temp} °C</Typography>
+            <Typography variant='body2'>High: {weather.main.temp_max} °C</Typography>
+            <Typography variant='body2'>Low: {weather.main.temp_min} °C</Typography>
+            <Typography variant='body2'>Humidity: {weather.main.humidity}%</Typography>
+          </CardContent>
+        </Card>
       )}
     </Container>
   )
